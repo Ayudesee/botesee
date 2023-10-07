c40431a0501f6e20ac9ed001beeb14bfc6fe144b
Ayudesee <sharaevvlad@mail.ru>
2023-10-07 17:18:42 +0300

    revert

--- bot/discord_bot/main.py
+++ bot/discord_bot/main.py
@@ -3,15 +3,10 @@ import asyncio
 from bot.clients.rabbit import RabbitWorker
 from bot.discord_bot import conf
 from bot.discord_bot.discord_factory import discord_factory
-from loguru import logger
 
 
 async def main():
     discord_client = discord_factory(conf.ENV)
-    logger.info(f"{conf.RABBIT_HOST = }")
-    logger.info(f"{conf.RABBIT_PORT = }")
-    logger.info(f"{conf.RABBIT_USER = }")
-    logger.info(f"{conf.RABBIT_PASSWORD = }")
     rabbit = RabbitWorker(
         conf.RABBIT_HOST,
         conf.RABBIT_PORT,

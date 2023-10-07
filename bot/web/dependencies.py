eb71446ac0fc08922607a904e2a85f410d261161
Ayudesee <sharaevvlad@mail.ru>
2023-10-07 16:12:43 +0300

    JT

--- bot/web/dependencies.py
+++ bot/web/dependencies.py
@@ -1,6 +1,11 @@
 from bot import conf
 from bot.clients.rabbit import RabbitClient
+from loguru import logger
 
 
 async def get_rabbit() -> RabbitClient:
+    logger.info(f"{conf.RABBIT_HOST = }")
+    logger.info(f"{conf.RABBIT_PORT = }")
+    logger.info(f"{conf.RABBIT_USER = }")
+    logger.info(f"{conf.RABBIT_PASSWORD = }")
     return RabbitClient(conf.RABBIT_HOST, conf.RABBIT_PORT, conf.RABBIT_USER, conf.RABBIT_PASSWORD)
